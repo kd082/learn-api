85f7a84874c0ef82d89c51b5acc22c99caa1c319
k.dutta <k.dutta@ruitenburg.nu>
2024-02-03 12:56:50 +0100

    Initial commit

---
+++ test/learn-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LearnApi from '../lib/learn-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/learn-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LearnApi.LearnApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
