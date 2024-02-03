c34720ca1a9a317976128780a4f96e460478f94f
k.dutta <k.dutta@ruitenburg.nu>
2024-02-03 21:01:41 +0100

    My first api

--- bin/learn-api.ts
+++ bin/learn-api.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { LearnApiStack } from '../lib/learn-api-stack';
 
 const app = new cdk.App();
-new LearnApiStack(app, 'LearnApiStack', {
+new LearnApiStack(app, 'LearnApiStack-k-dutta', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/learn-api-stack.ts
+++ lib/learn-api-stack.ts
@@ -1,16 +1,37 @@
 import * as cdk from 'aws-cdk-lib';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as path from 'path';
 
 export class LearnApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const apiSpec = apigateway.AssetApiDefinition.fromAsset('./services/api/api.yaml');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LearnApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+    const api = new apigateway.SpecRestApi(this, 'cars-api-k-dutta', {
+      apiDefinition: apiSpec,
+  });
+
+  const lambdaNames = [
+    { name: 'cars-get', handler: 'handler.lambda_handler' }
+  ];
+
+  lambdaNames.forEach((lambdaConfig) => {
+    const lambdaFn = new lambda.Function(this, lambdaConfig.name, {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      functionName: lambdaConfig.name,
+      handler: lambdaConfig.handler,
+      code: lambda.Code.fromAsset(path.join(__dirname, `../services/lambda/${lambdaConfig.name}`))
+    });
+
+    // Grant API Gateway permissions to invoke the Lambda function
+    lambdaFn.grantInvoke(new iam.ServicePrincipal('apigateway.amazonaws.com'));
+  });
 }
+}
+
+const app = new cdk.App();
+new LearnApiStack(app, 'ApiGatewayLambdaExample');
